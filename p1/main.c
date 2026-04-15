f74eaefc7db7181ff9b765f0b25fd57a60ff8443
2025129036 <azljycover3366@naver.com>
2026-04-15 15:35:35 +0900

    Add number guessing game in main.c
    Implement a number guessing game that compares user input with a predefined answer.

---
+++ p1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int cor = 0, num = 0;
+    scanf("정답 입력 : %d", &cor);
+    
+    do {
+        scanf("%d", &num);
+        if ( num > cor )
+            printf("%d>?", num);
+        else if ( num < cor )
+            printf("%d<?", num);
+        else
+            printf("%d==?", num);
+    } while ( num == cor );
+    
+    return 0;
+}
