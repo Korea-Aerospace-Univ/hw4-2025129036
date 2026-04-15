b26d83d8fd27ce55f6c42cf4c9260cfaf055f307
2025129036 <azljycover3366@naver.com>
2026-04-15 19:15:10 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
-    int cor = 0, num = 0;
+int main() {
+    int cor = 0, num = 0, cnt = 0;
     scanf("%d", &cor);
-    
+
     do {
         scanf("%d", &num);
-        if ( num > cor )
-            printf("%d>?", num);
-        else if ( num < cor )
-            printf("%d<?", num);
+        cnt++;
+
+        if (num > cor)
+            printf("%d>?\n", num);
+        else if (num < cor)
+            printf("%d<?\n", num);
         else
-            printf("%d==?", num);
-    } while ( num != cor );
-    
+            printf("%d==?\n", num);
+
+    } while (num != cor);
+
+    printf("%d\n", cnt);
+
     return 0;
 }
