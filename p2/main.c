d735416ca7810531d1c4d713deabb7990077c67f
2025129036 <azljycover3366@naver.com>
2026-04-15 19:17:33 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int N = 0, numcount = 0, strcount = 0, numfin = 0, strfin = 0;
+    int N = 0, numcount = 0, strcount = 0;
+    int numfin = 0, strfin = 0;
     char ch = 0;
-    
+
     scanf("%d", &N);
-    
-    for(int i = 1; i <= N; i++) {
+    getchar();
+
+    for (int i = 1; i <= N; i++) {
         scanf("%c", &ch);
-        
+
         if (ch >= 'a' && ch <= 'z') {
             strcount++;
             numcount = 0;
-            
+
             if (strcount > strfin)
                 strfin = strcount;
-        }    
-        else if (ch >= 0 && ch <= 9) {
+        }
+        else if (ch >= '0' && ch <= '9') {
             numcount++;
             strcount = 0;
-            
-            if (numfin < numcount)
+
+            if (numcount > numfin)
                 numfin = numcount;
         }
         else {
@@ -28,8 +30,9 @@ int main() {
             strcount = 0;
         }
     }
+
     printf("%d\n", strfin);
     printf("%d\n", numfin);
-    
+
     return 0;
 }
